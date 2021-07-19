b3f32eca4a9a1ebc5d8d848d526ce561fe45cf9b
Polomaru <paul.rios@utec.edu.pe>
2021-07-19 12:30:04 -0500

    config deployment

--- src/main/java/kitou/controller/UserController.java
+++ src/main/java/kitou/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
-@CrossOrigin(origins = CRest.FRONT_URI)
+@CrossOrigin("*")
 @RestController
 public class UserController {
 
